158c1a371380148d6f8613208be25513ab717b69
Kuzmych Max <m_kuzmych@ugi-studio.com>
2023-08-25 02:11:17 +0300

    InputEventsHandler

---
+++ Assets/Scripts/Game/Input/InputEventsHandler.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace ShatterShapes.Game.Input
+{
+    public static class InputEventsHandler
+    {
+        public static Action<Vector2> JoystickDirectionChanged;
+    }
+}
