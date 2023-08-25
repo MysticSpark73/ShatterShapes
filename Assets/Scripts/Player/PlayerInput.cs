293202f18dc224e890b82aca946d53dcfaa31ee5
Kuzmych Max <m_kuzmych@ugi-studio.com>
2023-08-25 22:53:23 +0300

    Input Fix

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -15,36 +15,15 @@ namespace ShatterShapes.Player
 
         private void Awake()
         {
-            InputEventsHandler.JoystickDirectionChanged += OnJoystickDirectionChanged;
             _touchDetector.AddListener(OnTouch);
-        }
-
-        private void OnApplicationQuit()
-        {
-            InputEventsHandler.JoystickDirectionChanged -= OnJoystickDirectionChanged;
-        }
-
-        private void OnJoystickDirectionChanged(Vector2 dir)
-        {
-            Debug.Log(dir);
-            if (dir == Vector2.zero) return;
-            _rotationX -= dir.x * _mouseSensitivity * Time.deltaTime;
-            _rotationY -= dir.y * _mouseSensitivity * Time.deltaTime;
-            _rotationX = Mathf.Clamp(_rotationX, -90, 90);
-            _rotationY = Mathf.Clamp(_rotationY, -45, 45);
-            transform.localRotation = Quaternion.Euler( 0, -_rotationX, 0);
-            _cameraTransform.localRotation = Quaternion.Euler(_rotationY, 0 , 0);
         }
 
         private void OnTouch()
         {
-            /*if (Input.GetMouseButton(0))
-            {*/
-                Debug.DrawRay(transform.position, _playerCamera.ScreenPointToRay(Input.mousePosition).direction,
-                    Color.red);
-                InputEventsHandler.PlayerFirePressed?.Invoke(_playerCamera.ScreenPointToRay(Input.mousePosition)
-                    .direction);
-            /*}*/
+            Debug.DrawRay(transform.position, _playerCamera.ScreenPointToRay(Input.mousePosition).direction,
+                Color.red);
+            InputEventsHandler.PlayerFirePressed?.Invoke(_playerCamera.ScreenPointToRay(Input.mousePosition)
+                .direction);
         }
     }
 }
--- Assets/Scripts/UI/OnPointerDownListener.cs
+++ Assets/Scripts/UI/OnPointerDownListener.cs
@@ -4,15 +4,30 @@ using UnityEngine.EventSystems;
 
 namespace ShatterShapes.UI
 {
-    public class OnPointerDownListener : MonoBehaviour, IPointerDownHandler
+    public class OnPointerDownListener : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         private Action _onPointerDownListener;
+        private bool _isPointerDown;
 
         public void AddListener(Action listener) => _onPointerDownListener = listener;
-        
+
+        private void Update()
+        {
+            if (_isPointerDown)
+            {
+                _onPointerDownListener?.Invoke();
+            }
+        }
+
         public void OnPointerDown(PointerEventData eventData)
         {
             _onPointerDownListener?.Invoke();
+            _isPointerDown = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _isPointerDown = false;
         }
     }
 }
